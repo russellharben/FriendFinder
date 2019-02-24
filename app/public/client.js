da25b8cc53da873dcc32628eeb85dea7adcc3d54
Russell H <russellharben@gmail.com>
2019-02-24 17:44:45 -0500

    stuff

--- app/public/client.js
+++ app/public/client.js
@@ -1,6 +1,42 @@
-$(".custom-control-label").on("click", function(){
-    alert("Test success");
-    if($(this).checked === true){
-        console.log($(this).val());
+
+
+function newFriend(name, photo, scoreArr) {
+    this.name = name;
+    this.photo = photo;
+    this.scoreArr = scoreArr;
+}
+
+$("form").on("submit", function(e){
+    e.preventDefault();
+    let inputs = $("input");
+    let scoreArr = [];
+
+    var name = $("#name").val().trim();
+    var photo = $("#photo").val().trim();
+    
+    var currentFriend = new newFriend(name, photo, scoreArr);
+    console.log("Current friend is : ", currentFriend);
+
+    console.log(inputs);
+    for(i = 0; i < inputs.length; i++){
+        if(inputs[i].checked === true){
+            scoreArr.push(inputs[i].value);
+        }
     }
-}); 
+
+    $.ajax({
+        method: "POST",
+        url: "/api/friends",
+        data: currentFriend,
+        success: "success"
+      })
+        .done(function(data) {
+          console.log("This is the friends array ", data);
+          data.push(currentFriend);
+        });
+});
+
+
+
+
+
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,6 @@
 var friendData = require('../data/friends');
-module.exports = function (app) {
 
-    
+module.exports = function (app) {
 
     app.get('/api/friends', function (req, res) {
         res.send(friendData);
@@ -9,7 +8,7 @@ module.exports = function (app) {
 
     app.post('/api/friends', function (req, res) {
         friendData.push(req.body);
+        res.send(friendData);
     });
-
 };
 
