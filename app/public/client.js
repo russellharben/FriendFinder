b6935bfb62296263d6b979e705f13ed22a592686
Russell H <russellharben@gmail.com>
2019-02-24 19:50:42 -0500

    progress

--- app/public/client.js
+++ app/public/client.js
@@ -16,16 +16,18 @@ $("form").on("submit", function (e) {
     var currentFriend = new newFriend(name, photo, scoreArr);
     console.log("Current friend is : ", currentFriend);
 
-    console.log("INPUTS = ", inputs);
+    console.log(currentFriend.scoreArr);
+
 
     for (i = 0; i < inputs.length; i++) {
         if (inputs[i].checked === true) {
             scoreArr.push(inputs[i].value);
+            console.log("Yes");
         }
     }
 
     // passing the array of scores to the friendScore function
-    friendScore(scoreArr);
+    compareScores(scoreArr);
 
     // making a post request to the /api/friends path in the apiRoutes.js file
     $.ajax({
@@ -36,22 +38,30 @@ $("form").on("submit", function (e) {
     })
         .done(function (data) {
             console.log("This is the friends array ", data);
-            console.log(typeof data);
+
             let newFriendsArr = data;
-            for (i = 0; i < newFriendsArr.length - 1; i++) {
+
+            let currentScores = [];
+
+            for (i = 0; i < newFriendsArr.length -1; i++) {
                 let totals = 0;
                 var eachScoreArr = newFriendsArr[i].scores;
                 // console.log("each score array = ", eachScoreArr);
                 for(j = 0; j < eachScoreArr.length; j++){
                     totals += eachScoreArr[j];
+                    
                 }
-                console.log("Totals = ", totals);
+                currentScores.push(totals);
+                console.log("Total Score for " + newFriendsArr[i].name + " = ", totals);
+                console.log(currentScores, "Current scores");
             }
+
+            // compareScores( ,currentScores);
         });
 });
 
 
 
-function compareScores(friendScores, newScore) {
+function compareScores(friendScores, totals) {
 
 }
